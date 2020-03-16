5b0b1e8c1160d9c2887cd578c068b53a921e9e8a
Rojer7886 <pairoj.kiatpanya@kellogg.northwestern.edu>
2020-03-16 16:18:29 +0000

    a meaningful commit message

--- app.rb
+++ app.rb
@@ -14,5 +14,143 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
+restaurants_table = DB.from(:restaurants)
 rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+reviews_table = DB.from(:reviews)
+
+
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+get "/" do
+    puts "params: #{params}"
+
+    pp restaurants_table.all.to_a
+    @restaurants = restaurants_table.all.to_a
+    view "restaurants"
+end
+
+get "/restaurants/:id" do
+    puts "params: #{params}"
+
+    # @users_table = users_table
+    @restaurant = restaurants_table.where(id: params[:id]).to_a[0]
+    @review = reviews_table.where(restaurant_id: @restaurant[:id]).to_a
+    @average = reviews_table.where(restaurant_id: @restaurant[:id]).avg(:scores)
+    @lat_long = "#{@restaurant[:lat]},#{@restaurant[:long]}"
+    @google = ENV["GoogleAPI"]
+    view "restaurant"
+end
+
+post "/restaurants/:id/rsvps/create" do
+    puts "params: #{params}"
+
+    # first find the event that rsvp'ing for
+    @restaurant = restaurants_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvps table with the rsvp form data
+    rsvps_table.insert(
+        restaurant_id: @restaurant[:id],
+        user_id: session["user_id"],
+        date: params["date"],
+        time: params["Time"],
+        number_of_ppl: params["#people"],
+        comments: params["comments"]
+    )
+
+    accout_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH"]
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    client.messages.create(
+    from: "+12056515499", 
+    to: @current_user[:phone_number],
+    body: "Your booking details:"
+    )
+
+    redirect "/restaurants/#{@restaurant[:id]}"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts "params: #{params}"
+
+
+    # if there's already a user with this email, skip!
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
+    users_table.insert(
+        name: params["name"],
+        phone_number: params["phone"],
+        email: params["email"],
+        password: BCrypt::Password.create(params["password"])
+
+    )
+        redirect "/logins/new"
+    end
+    view "create_user"
+end
+
+get "/logins/new" do
+
+
+    view "new_login"
+end
+
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            redirect "/"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    redirect "/logins/new"
+end
+
+get "/restaurants/:id/writeareview/new" do
+    puts "params: #{params}"
+
+    # @users_table = users_table
+    @restaurant = restaurants_table.where(id: params[:id]).to_a[0]
+
+    view "writeareview"
+end
+
+post "/restaurants/:id/writeareview/create" do
+    puts "params: #{params}"
+
+    # first find the event that rsvp'ing for
+    @restaurant = restaurants_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvps table with the rsvp form data
+    reviews_table.insert(
+        restaurant_id: @restaurant[:id],
+        user_id: session["user_id"],
+        scores: params["exampleRadios"],
+        comments: params["comments"]
+    )
+
+    redirect "/restaurants/#{@restaurant[:id]}"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,57 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :restaurants do
   primary_key :id
-  String :title
+  String :name
   String :description, text: true
-  String :date
-  String :location
+  String :contact
+  String :address
+  String :website
+  String :lat
+  String :long
 end
 DB.create_table! :rsvps do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
-  String :name
-  String :email
+  foreign_key :restaurant_id
+  foreign_key :user_id
+  String :date
+  String :time
+  Integer :number_of_ppl
   String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :phone_number
+  String :email
+  String :password
+end
+DB.create_table! :reviews do
+  primary_key :id
+  foreign_key :restaurant_id
+  foreign_key :user_id
+  Float :scores
+  String :comments
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+restaurants_table = DB.from(:restaurants)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+restaurants_table.insert(name: "Peppercorns", 
+                    description: "Peppercorns Kitchen is a chef-driven contemporary Chinese restaurant located at Evanston, IL. Our mission is to deliver a brand-new culinary experience mixing authentic Szechuan cuisine and creative American-Chinese fusion.",
+                    contact: "(847) 563-8461",
+                    address: "620 Davis St Peppercorns Kitchen, Evanston, IL  60201 USA",
+                    website: "https://peppercornskitchen.com/",
+                    lat: "42.046751", 
+                    long: "-87.680503")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+restaurants_table.insert(name: "Red Hot Chilli Pepper", 
+                    description: "We are part of the highly acclaimed and hugely popular “Red Hot Chilli Pepper Group” based in Kolkata, India.
+Aptly named “Red Hot Chilli Pepper”, we take inspiration from the vibrant flavors of Asia, using fresh produce, meat, seafood and poultry with the aim to provide you with an exceptional dining experience!",
+                    contact: "(847) 563-8085",
+                    address: "500 Davis St, Evanston, IL 60201, USA",
+                    website: "http://rhcponline.com/",
+                    lat: "42.046023",
+                    long: "-87.678719")
+
